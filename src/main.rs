ec318b85dcebdec2a1fb4a2187104dcd9aeb892a
Ivan Smirnov <i.s.smirnov@gmail.com>
2019-09-23 23:47:08 +0100

    Perf improvements (depth=7: 2.38s, depth=6: 0.29s)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use std::time::SystemTime;
 
 fn main() {
-    let board = Bitboard::default();
     benchmark("movegen", || {
-        println!("{}", move_gen(board, 7));
+        println!("{}", move_gen(7));
     });
 }
 
@@ -22,146 +21,128 @@ where
     );
 }
 
-pub const FIELD: usize = 0x1FF << 16;
-pub const SQUARE: usize = 0xFFFF;
-pub const ALL_FIELDS_LEGAL: usize = 0x1 << 25;
+type Index = u8;
+type Bits = u16;
 
-pub const WIN: [usize; 8] = [0o421, 0o124, 0o700, 0o070, 0o007, 0o111, 0o222, 0o444];
-pub const ALL_FIELDS: usize = 0o777;
+pub const WIN: [Bits; 8] = [0o421, 0o124, 0o700, 0o070, 0o007, 0o111, 0o222, 0o444];
+pub const ALL_FIELDS: Bits = 0o777;
 
-#[macro_export]
-macro_rules! field {
-    ($val:expr) => {
-        ($val & FIELD) >> 16
-    };
+#[derive(Copy, Clone, Default)]
+pub struct Pos {
+    pub field: Index,
+    pub all_legal: bool,
+    pub square: Bits,
 }
 
-#[macro_export]
-macro_rules! square {
-    ($val:expr) => {
-        $val & SQUARE
-    };
-}
-
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Default)]
 pub struct Bitboard {
-    pub valid_field: usize,
-    pub board: [[usize; 9]; 2],
+    pub valid_field: Option<Index>,
+    pub board: [[Bits; 9]; 2],
     pub turn: usize,
-    cached_meta_field: [usize; 2],
-    cached_meta_field_dirty: bool,
-    cached_game_over: bool,
-    cached_game_over_dirty: bool,
-}
-
-impl Default for Bitboard {
-    fn default() -> Self {
-        Bitboard {
-            valid_field: ALL_FIELDS,
-            board: [[0; 9]; 2],
-            turn: 0,
-            cached_meta_field: [0; 2],
-            cached_game_over: false,
-            cached_meta_field_dirty: true,
-            cached_game_over_dirty: true,
-        }
-    }
+    meta_field: Option<[Bits; 2]>,
+    game_over: Option<bool>,
 }
 
 impl Bitboard {
     fn dirty(&mut self) {
-        self.cached_game_over_dirty = true;
-        self.cached_meta_field_dirty = true;
+        self.meta_field = None;
+        self.game_over = None;
     }
 
-    fn taken(&self, pos: usize) -> bool {
-        (self.board[0][to_index(field!(pos))] | self.board[1][to_index(field!(pos))]) & square!(pos)
-            != 0
+    fn get(&self, p: usize, field: Index) -> Bits {
+        let f = field as usize;
+        unsafe { *self.board.get_unchecked(p).get_unchecked(f) }
     }
 
-    pub fn make_move(&mut self, mov: usize) {
-        let field = field!(mov);
-        let square = square!(mov);
-        self.board[self.turn][to_index(field)] |= square;
-        if self.field_is_blocked(to_index(square)) || is_won(field) {
-            self.valid_field = ALL_FIELDS;
+    fn get_mut(&mut self, p: usize, field: Index) -> &mut Bits {
+        let f = field as usize;
+        unsafe { self.board.get_unchecked_mut(p).get_unchecked_mut(f) }
+    }
+
+    fn get_fields(&self, field: Index) -> (Bits, Bits) {
+        (self.get(0, field), self.get(1, field))
+    }
+
+    pub fn make_move(&mut self, mov: Pos) {
+        *self.get_mut(self.turn, mov.field) |= mov.square;
+        self.valid_field = if self.field_is_blocked(mov.field) {
+            None
         } else {
-            self.valid_field = square;
-        }
+            Some(mov.square.trailing_zeros() as _)
+        };
         self.turn = 1 - self.turn;
         self.dirty();
     }
 
-    pub fn get_all_moves(&self) -> Vec<usize> {
-        let mut list = Vec::new();
-
-        for i in 0..9 {
-            if (1 << i) & self.valid_field != 0 {
-                for s in 0..9 {
-                    let mov = (1 << s) | ((1 << i) << 16);
-                    if !self.taken(mov) && !self.field_is_blocked(i) {
-                        list.push(
-                            mov | (if self.valid_field == 0o777 {
-                                ALL_FIELDS_LEGAL
-                            } else {
-                                0
-                            }),
-                        );
-                    }
+    pub fn get_all_moves(&self, moves: &mut Vec<Pos>) -> usize {
+        let all_legal = self.valid_field.is_none();
+        let mut n_moves = 0;
+        let available_fields = match self.valid_field {
+            Some(field) => field..field + 1,
+            _ => 0..9,
+        };
+        for field in available_fields {
+            let (white, black) = self.get_fields(field);
+            let any = white | black;
+            let blocked = is_won(white) | is_won(black) | is_tied(any);
+            if blocked {
+                continue;
+            }
+            for square in 0..9 {
+                let square = 1 << square;
+                let taken = any & square != 0;
+                if taken {
+                    continue;
                 }
+                moves.push(Pos {
+                    field,
+                    all_legal,
+                    square,
+                });
+                n_moves += 1;
             }
         }
-        return list;
+        n_moves
     }
 
-    pub fn undo_move(&mut self, mov: usize) {
-        let field = field!(mov);
-        let square = square!(mov);
-        self.board[1 - self.turn][to_index(field)] &= !square;
-        if mov & ALL_FIELDS_LEGAL != 0 {
-            self.valid_field = ALL_FIELDS;
-        } else {
-            self.valid_field = field;
-        }
+    pub fn undo_move(&mut self, mov: Pos) {
+        *self.get_mut(1 - self.turn, mov.field) &= !mov.square;
+        self.valid_field = if mov.all_legal { None } else { Some(mov.field) };
         self.turn = 1 - self.turn;
         self.dirty();
     }
 
-    fn field_is_blocked(&self, field: usize) -> bool {
-        let white_field = self.board[0][field];
-        let black_field = self.board[1][field];
-        is_won(white_field) || is_won(black_field) || is_tied(white_field | black_field)
+    fn field_is_blocked(&self, field: Index) -> bool {
+        let (white, black) = self.get_fields(field);
+        is_won(white) || is_won(black) || is_tied(white | black)
     }
 
-    pub fn get_meta_field(&mut self) -> [usize; 2] {
-        if !self.cached_meta_field_dirty {
-            return self.cached_meta_field;
+    pub fn get_meta_field(&mut self) -> [u16; 2] {
+        if let Some(field) = self.meta_field {
+            return field;
         }
         let mut field = [0; 2];
         for p in 0..2 {
             field[p] = (0..9)
-                .map(|i| (is_won(self.board[p][i]) as usize) << (8 - i))
+                .map(|i| (is_won(self.board[p][i]) as u16) << (8 - i as u16))
                 .fold(0, |x, y| x | y);
         }
-        self.cached_meta_field = field;
-        self.cached_meta_field_dirty = false;
+        self.meta_field = Some(field);
         field
     }
 
     pub fn game_over(&mut self) -> bool {
-        if !self.cached_game_over_dirty {
-            return self.cached_game_over;
+        if let Some(game_over) = self.game_over {
+            return game_over;
         }
-
         let sum = (0..9).fold(0, |acc, i| acc + self.board[0][i].count_ones());
         if sum < 9 {
             return false;
         }
         let meta_field = self.get_meta_field();
-        let ret = is_won(meta_field[0]) | is_won(meta_field[1]) || self.game_tied();
-        self.cached_game_over = ret;
-        self.cached_game_over_dirty = false;
-        return ret;
+        let game_over = is_won(meta_field[0]) | is_won(meta_field[1]) || self.game_tied();
+        self.game_over = Some(game_over);
+        game_over
     }
 
     pub fn game_tied(&self) -> bool {
@@ -173,36 +154,39 @@ impl Bitboard {
     }
 }
 
-pub fn to_index(value: usize) -> usize {
-    value.trailing_zeros() as _
-}
-
-pub fn is_tied(field: usize) -> bool {
+pub fn is_tied(field: Bits) -> bool {
     field == ALL_FIELDS
 }
 
-pub fn is_won(field: usize) -> bool {
+pub fn is_won(field: Bits) -> bool {
     WIN.iter().any(|&w| field & w == w)
 }
 
-pub fn move_gen(mut board: Bitboard, depth: usize) -> usize {
+pub fn move_gen_impl(board: &mut Bitboard, depth: usize, moves: &mut Vec<Pos>) -> usize {
     if board.game_over() {
         return 0;
     } else {
-        let moves = board.get_all_moves();
-        return moves.len()
-            + if depth > 0 {
-                moves
-                    .iter()
-                    .map(|mov| {
-                        board.make_move(*mov);
-                        let ret = move_gen(board, depth - 1);
-                        board.undo_move(*mov);
-                        ret
-                    })
-                    .sum()
-            } else {
-                0
-            };
+        let n_moves = board.get_all_moves(moves);
+        let mut sum = n_moves;
+        if depth > 0 {
+            for _ in 1..=n_moves {
+                let mov = moves.pop().unwrap();
+                board.make_move(mov);
+                let n = move_gen_impl(board, depth - 1, moves);
+                board.undo_move(mov);
+                sum += n;
+            }
+        } else {
+            for _ in 0..n_moves {
+                let _ = moves.pop();
+            }
+        }
+        sum
     }
+}
+
+pub fn move_gen(depth: usize) -> usize {
+    let mut bitboard = Default::default();
+    let mut moves = Vec::with_capacity(1 << 16);
+    move_gen_impl(&mut bitboard, depth, &mut moves)
 }
